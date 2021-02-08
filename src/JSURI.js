c03a94cd9f45d47e6e16c7346f83979afeddff2e
Serhii Khoma <srghma@gmail.com>
2021-02-08 21:46:32 +0200

    fix: typo

--- src/JSURI.js
+++ src/JSURI.js
@@ -1,10 +1,10 @@
 "use strict";
 
 // A helper which transforms the result ofencodeURIComponent to be compliant
-// with RFC3896, as described in the MDN documentation here:
+// with RFC3986, as described in the MDN documentation here:
 //
 // https://web.archive.org/web/20201206001047/https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent
-function toRFC3896(input) {
+function toRFC3986(input) {
   return input.replace(/[!'()*]/g, function (c) {
     return "%" + c.charCodeAt(0).toString(16);
   });
@@ -12,7 +12,7 @@ function toRFC3896(input) {
 
 exports._encodeURIComponent = function _encodeURIComponent(fail, succeed, input) {
   try {
-    return succeed(toRFC3896(encodeURIComponent(input)));
+    return succeed(toRFC3986(encodeURIComponent(input)));
   } catch (err) {
     return fail(err);
   }
@@ -20,7 +20,7 @@ exports._encodeURIComponent = function _encodeURIComponent(fail, succeed, input)
 
 exports._encodeFormURLComponent = function _encodeFormURLComponent(fail, succeed, input) {
   try {
-    return succeed(toRFC3896(encodeURIComponent(input)).replace(/%20/g, "+"));
+    return succeed(toRFC3986(encodeURIComponent(input)).replace(/%20/g, "+"));
   } catch (err) {
     return fail(err);
   }
